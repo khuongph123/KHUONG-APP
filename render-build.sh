580f480cf096eed6756d953b90fbe363f0617e30
khuongph123 <57988857+khuongph123@users.noreply.github.com>
2025-10-08 17:57:37 +0700

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Cài đặt các thư viện từ package.json
-npm install
-
-# Cài đặt trình duyệt cho Puppeteer
-npx puppeteer browsers install chrome
