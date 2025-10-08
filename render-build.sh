7f76a665e74353befb5aecb0f7822f25e3095982
khuongph123 <57988857+khuongph123@users.noreply.github.com>
2025-10-08 17:40:49 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,12 +2,6 @@
 # exit on error
 set -o errexit
 
-# Tăng bộ nhớ ảo (swap) để Puppeteer chạy ổn định trên gói Free
-fallocate -l 1G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-
 # Cài đặt các thư viện từ package.json
 npm install
 
